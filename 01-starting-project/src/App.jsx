ebdc756a8eeec66ceb9b0dab78b3cce690a27bb4
Aries <shinehtet.has99@gmail.com>
2024-11-25 13:37:55 +0700

    [01] folderStructure

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,7 +1,7 @@
 
 import { CORE_CONCEPTS } from './data';
 import Header from './components/Header/Header';
-import CoreConcepts from './components/CoreConcepts';
+import CoreConcepts from './components/CoreConcepts/CoreConcepts';
 
 function App() {
   return (
